1b42645acff625baffdc697edd66f7d2cd344657
Saumya45 <125527285+Saumya45@users.noreply.github.com>
2023-02-20 23:43:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,10 @@ streamlit.multiselect("pick some fruits :",list (my_fruit_list.index),['Avocado'
 
 streamlit.header("Fruityvice Fruit Advice!")
 streamlit.header("Fruityvice Fruit Advice!")
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
