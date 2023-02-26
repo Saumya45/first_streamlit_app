3c53515c2718a78f192c75c48e51e7316a0bd89f
Saumya45 <125527285+Saumya45@users.noreply.github.com>
2023-02-26 14:17:02 +0800

    ws

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,11 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.text(my_data_rows)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 
 
 def insert_row_snowflake(new_fruit):
      with my_cnx.cursor() as my_cur:
-      my_cur.execute("insert into fruit_load_list values ('Jackfruit in jungle')")
+      my_cur.execute("insert into fruit_load_list values ('"+ new_fruit +"')")
       return "Thanks for adding" + new_fruit
